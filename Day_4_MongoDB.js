6d13248f896697982a25af755cafb92857254eef
Phyo Hein ( Ezekiel ) <mwrcps11@gmail.com>
2025-11-16 21:33:30 +0700

    Impl Category Collection

--- Day_4_MongoDB.js
+++ Day_4_MongoDB.js
@@ -1,25 +1,51 @@
 
 require('dotenv').config();
+//Require
 const mongoose = require('mongoose');
     express = require('express');
     bodyParser = require('body-parser');
     app = express();
     fileUpload = require('express-fileupload');
+const {saveFile} = require("./utils/gallery");
+//Usage
+mongoose.connect(process.env.MONGO_URL + process.env.DB_NAME);
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(fileUpload());
 
-    mongoose.connect(process.env.MONGO_URL + process.env.DB_NAME);
+//Schema
 const { Schema } = mongoose;
 const CategorySchema = new Schema({
     name: {type: String, unique: true, required: true},
     image: {type: String, required: true},
+    createdAt : {type : Date, default :  Date.now()}
+})
+
+const Category = mongoose.model("category", CategorySchema);
+
+app.post("/category", saveFile , async (req, res,next) => {
+    let saveCategory = new Category(req.body);
+    let result = await saveCategory.save();
+    res.status(200).json({
+        con : true,
+        msg : "Category Created",
+        result : result
+    });
+})
+
+app.get("/category", async (req, res) => {
+    let category = await Category.find();
+    res.status(200).json({
+        con : true,
+        msg : "All Categories",
+        result : category
+    })
 })
 
 
-app.use(express.json());
-app.use(bodyParser.json());
-
 app.use((err, req, res,next) => {
     err.status = err.status || 404;
-    res.status(err.status),send({con : false, msg : err.message});
+    res.status(err.status).send({con : false, msg : err.message});
 })
 
 app.use((req, res) => {
