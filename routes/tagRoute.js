67b1cc72558e940ab50b2fcb4b687af942385adb
Phyo Hein ( Ezekiel ) <mwrcps11@gmail.com>
2025-12-07 23:28:12 +0700

    Impl tagCategory

--- routes/tagRoute.js
+++ routes/tagRoute.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const controller = require('../controllers/tagController');
 const {saveFile } = require('../utils/gallery');
-const {TagSchema} = require('../utils/schema');
-const {validateBody} = require('../utils/validator');
+const {TagSchema, AllSchema} = require('../utils/schema');
+const {validateBody, validateParams} = require('../utils/validator');
 
 router.get('/', controller.getAllTags)
-router.get('/:id',controller.getSingleTag)
+router.get('/:id',validateParams(AllSchema.id, 'id') , controller.getSingleTag)
 router.post('/', saveFile, validateBody(TagSchema.add), controller.addTag)
 router.patch('/:id',saveFile, validateBody(TagSchema.update), controller.updateTag)
 router.delete('/:id', controller.deleteTag)
--- utils/validator.js
+++ utils/validator.js
@@ -8,6 +8,18 @@ module.exports = {
                 next()
             }
         }
+    },
+    validateParams : (schema , name) => {
+        return (req, res, next) => {
+            let obj = {}
+            obj[`${name}`] = req.params[`${name}`];
+            let result = schema.validate(obj);
+            if(result.error){
+                next(new Error(result.error.details[0].message))
+            }else{
+                next()
+            }
+        }
     }
 
 }
